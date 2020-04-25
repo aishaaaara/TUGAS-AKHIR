58853983052321fcd486749044200c80dd52110a
Aisha Rahmadia Aqilah <63826055+aishaaaara@users.noreply.github.com>
2020-04-25 11:26:07 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,2 @@
+<script type="http://localhost:8080/myweb/aset/jquery.js"></script>
+<script type="http://localhost:8080/myweb/aset/boothstrap/js/boothstrap.min.js"></script>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Comapatible" content="ie=edge">
+
+<link rel="stylesheet" type="text/css" href="http://localhost:8080/myweb/aset/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="http://localhost:8080/myweb/aset/fontawesome/css/all.min.css">	
+    
+    <title>myweb</title>
+</head>
+<body>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <a class="navbar-brand" href="#"><i class="fas fa-user-nurse"></i> MONITORING | Hai,Petugas</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+  aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost:8080/myweb/dashboard.php">Dashboard</a>
+      <a class="nav-item nav-link" href="http://localhost:8080/myweb/index.php">More</a>
+      <a class="nav-item nav-link" href="http://localhost:8080/myweb/pasien/pasien.php">Pasien</a>
+      <a class="nav-item nav-link" href="http://localhost:8080/myweb/hal1.html">Logout</a>
+      
+
+    </div>
+  </div>
+</nav>
+
+
+</body>
+</html>
