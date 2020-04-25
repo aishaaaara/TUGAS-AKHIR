762a06f53afd56525dbe1ad7cc258cb07b0afa1b
Aisha Rahmadia Aqilah <63826055+aishaaaara@users.noreply.github.com>
2020-04-25 11:24:56 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,67 @@
+<?php 
+include 'aset/header.php'; 
+?>
+
+    <div class="container">
+	<div class="row mt-4">
+    <div class="col-md">
+			<h2><i class="fas fa-exclamation-triangle"> Dashboard</h2></i>
+			<hr class="bg-light">
+		</div>
+        </div>
+        <di
+	<div class="row">
+	<div class="col-md-4">
+		<div class="card bg-info" style="width: 18rem; border-radius: 30px" >
+        <div class="card-body text-white">
+        <h5 class="card-title">Jumlah ODP</h5>
+        <p class="card-text" style="font-size:60px">197.951</p>
+        <a href="#" class="text-white"> Dalam Pemanatauan  </i></a>
+       </div>
+       </div>
+	   </div>
+    <div class="col-md-4">
+         <div class="card bg-warning" style="width: 18rem; border-radius: 30px">
+         <div class="card-body text-white">
+         <h5 class="card-title">Jumlah PDP</h5>
+         <p class="card-text" style="font-size:60px">18.301</p>
+         <a href="#" class="text-white"<i class="fas fa-angel-double-right"> Dalam Pengawasan </i></a>
+     </div>
+     </div>
+     </div>
+    <div class="col-md-4">
+    	<div class="card bg-danger" style="width: 18rem; border-radius: 30px">
+        <div class="card-body text-white">
+        <h5 class="card-title">Jumlah POSITIF</h5>
+        <p class="card-text" style="font-size:60px">8.211</p>
+        <a href="#" class="text-white"<i class="fas fa-angel-double-right"> Confirm </i></a>
+    </div>
+    </div>
+    </div>
+    	
+    </div>
+	</div>
+    </div>
+
+    <div class="container">
+    <div class="row mt-4">
+    <div class="col-md">
+
+<div class="row">
+<div class="col-md-4">
+        <div class="card bg-primary" style="width: 18rem; border-radius: 30px" >
+        <div class="card-body text-white">
+        <h5 class="card-title">Jumlah Sembuh</h5>
+        <p class="card-text" style="font-size:60px">1.002</p>
+        <a href="#" class="text-white"> Sembuh </i></a>
+       </div>
+       </div>
+       </div>
+
+   </div>
+</div>
+</div>
+</div>
+</div>
+<?php 
+include 'aset/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+include 'aset/header.php'; 
+?>
+
+    <div class="container">
+	<div class="row mt-4">
+    <div class="col-md">
+			<h2><i class="fas fa-bullhorn"> Info</h2></i>
+			<div class="p-3 mb-2 bg-info text-white"><center> INFO ABOUT COVID-19</center></div>
+           <B> Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special<br> treatment.  Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease,<br>
+and cancer are more likely to develop serious illness.<br><br>
+
+The best way to prevent and slow down transmission is be well informed about the COVID-19 virus, the disease it causes and how it spreads.<br>Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face. 
+<br><br>
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes.<br>
+ so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+
+At this time, there are no specific vaccines or treatments for COVID-19. However, there are many ongoing clinical trials evaluating potential treatments. WHO will continue to provide updated information as soon as clinical findings become available.
+<br><br>
+
+<p class="text-danger"> FROM : WORLD HEATLY ORGANITATION</p></B>
+<div class="alert alert-primary" role="alert">
+Check in this : <a href="https://www.who.int/health-topics/coronavirus" class="alert-link">     WHO CORONAVIRUS </a> 
+
+
+        </div>
+        </div>
+	
+    </div>
+    </div>
+    </div>
+    	
+    </div>
+	</div>
+    </div>
+
+
+<?php 
+include 'aset/footer.php'; ?>
